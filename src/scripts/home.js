2e63724bdc7f1384f9ccb85d02b2ee303eaab668
Santiago <santiagopujol7@gmail.com>
2026-03-10 16:03:54 +0000

    dynamic product template

---
+++ src/scripts/home.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    let response = await fetch("http://localhost:3000/products");
+    let products = await response.json();
+
+    let response2 = await fetch("../../templates/user/article-template.html");
+    const template = await response2.text();
+
+    const container = document.getElementById('grid-productos');
+    for (let product of products) {
+        container.innerHTML += createProduct(template, product)
+    }
+});
+
+function createProduct(template, product) {
+    return template.replace(/{{name}}/g, product.name)
+        .replace(/{{price}}/g, product.price / 100)
+        .replace(/{{img}}/g, product.img)
+}
