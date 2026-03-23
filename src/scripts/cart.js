e96c68b8838a3c386d550bc30617a3a31cf58a1f
Santiago <santiagopujol7@gmail.com>
2026-03-23 19:51:25 +0000

    add shopping cart

---
+++ src/scripts/cart.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadCartItems()
+})
+
+async function loadCartItems() {
+    let cartItems = JSON.parse(localStorage.getItem('cartItems'))
+
+    let response = await fetch("../../templates/user/cart-item.html");
+    const template = await response.text();
+
+    const container = document.getElementById('cart-box');
+
+    for (let cartItem of cartItems) {
+        let res = await fetch("http://localhost:3000/products/" + cartItem.id)
+        console.log(res)
+        const product = await res.json()
+        container.innerHTML += createCartItem(template, product, cartItem)
+    }
+
+
+}
+
+function createCartItem(template, product, cartItem) {
+    return template.replace(/{{name}}/g, product.name)
+        .replace(/{{id}}/g, product.id)
+        .replace(/{{price}}/g, product.price / 100)
+        .replace(/{{img}}/g, product.img)
+        .replace(/{{quantity}}/g, cartItem.quantity)
+}
+
