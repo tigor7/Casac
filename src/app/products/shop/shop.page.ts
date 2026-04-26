ed13c2685efeb4dfba86533f6b4e86321088c7c1
Santiago <santiagopujol7@gmail.com>
2026-04-26 14:52:37 +0100

    display products dynamically in shop page

--- src/app/products/shop/shop.page.ts
+++ src/app/products/shop/shop.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '@app/shared/components/header/header.component';
 import { FooterComponent } from '@app/shared/components/footer/footer.component';
 import { ProductCardComponent } from '@app/products/card/product-card.component';
+import { ProductService } from '../product.service';
+import { Product } from '../product.model';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     selector: 'app-shop-page',
-    imports: [HeaderComponent, FooterComponent, ProductCardComponent],
+    imports: [HeaderComponent, FooterComponent, ProductCardComponent, AsyncPipe],
     templateUrl: './shop.page.html',
     styleUrl: './shop.page.css',
 })
-export class ShopPage {}
+export class ShopPage implements OnInit {
+    products$ = new Observable<Product[]>();
+    constructor(private service: ProductService) {}
+
+    ngOnInit(): void {
+        this.products$ = this.service.getProducts();
+    }
+}
