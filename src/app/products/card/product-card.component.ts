d7d922a7db927cc10b4bb8523a51702fda9ff513
Santiago <santiagopujol7@gmail.com>
2026-04-26 19:32:26 +0100

    format currency with pipe instead

--- src/app/products/card/product-card.component.ts
+++ src/app/products/card/product-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../product.model';
 import { RouterLink } from '@angular/router';
+import { FormatCurrencyPipe } from '@app/shared/pipes/format-currency.pipe';
 
 @Component({
     selector: 'app-product-card',
-    imports: [RouterLink],
+    imports: [RouterLink, FormatCurrencyPipe],
     templateUrl: './product-card.component.html',
     styleUrl: './product-card.component.css',
 })
