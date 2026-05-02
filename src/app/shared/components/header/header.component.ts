75d6b202ecd5bdcb3572612ca1ae98bb0b636f66
Santiago <santiagopujol7@gmail.com>
2026-05-02 13:51:56 +0100

    add user state to header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,23 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '@app/auth/auth.service';
 import { CartService } from '@app/cart/cart-service';
 
 @Component({
     selector: 'app-header',
-    imports: [RouterLink],
+    imports: [RouterLink, AsyncPipe],
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+    private authService = inject(AuthService);
     private cartService = inject(CartService);
+
     numItems = computed(() => this.cartService.items().length);
+    user$ = this.authService.user$;
+
+    logout() {
+        this.authService.logout();
+    }
 }
