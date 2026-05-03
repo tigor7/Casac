fac6ae28e776d73294ecfde31393d3e94777be68
Javier <javiergonzavelazquez@gmail.com>
2026-05-03 22:20:57 +0100

    add environment configuration for Firebase

---
+++ src/enviroments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: 'AIzaSyBK4xbf1Vn9PIP3IxvD0HES3alHLihKA9A',
+        authDomain: 'casac-f6ebf.firebaseapp.com',
+        databaseURL: 'https://casac-f6ebf-default-rtdb.firebaseio.com',
+        projectId: 'casac-f6ebf',
+        storageBucket: 'casac-f6ebf.firebasestorage.app',
+        messagingSenderId: '797074636125',
+        appId: '1:797074636125:web:dd47aa815841d3240dfcb4',
+    },
+};
